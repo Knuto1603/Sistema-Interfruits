5be1741e79701dfe4b890d6863fbc46bdeaed669
Knuto1603 <108632698+Knuto1603@users.noreply.github.com>
2025-06-30 14:38:27 -0500

    Add filtering and pagination methods to ProductorRepository; implement list endpoint in productorApi

--- apps/core/src/Controller/productorApi.php
+++ apps/core/src/Controller/productorApi.php
@@ -5,14 +5,27 @@ namespace App\apps\core\Controller;
 use App\apps\core\Service\Productor\CreateProductorService;
 use App\apps\core\Service\Productor\Dto\ProductorDto;
 use App\apps\core\Service\Productor\Dto\ProductorDtoTransformer;
+use App\apps\core\Service\Productor\GetProductoresService;
 use App\shared\Api\AbstractSerializerApi;
+use App\shared\Service\Dto\FilterDto;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/productores')]
 class productorApi extends AbstractSerializerApi
 {
+
+    #[Route('/', name: 'productor_list', methods: ['GET'])]
+    public function list(
+        #[MapQueryString]
+        FilterDto $filterDto,
+        GetProductoresService $parametrosService,
+    ): Response {
+        return $this->ok($parametrosService->execute($filterDto));
+    }
+
     #[Route('/', name: 'productor_create', methods: ['POST'])]
     public function create(
         #[MapRequestPayload]
--- apps/core/src/Repository/ProductorRepository.php
+++ apps/core/src/Repository/ProductorRepository.php
@@ -4,6 +4,9 @@ namespace App\apps\core\Repository;
 
 use App\apps\core\Entity\Productor;
 use App\shared\Doctrine\DoctrineEntityRepository;
+use App\shared\Repository\PaginatorInterface;
+use App\shared\Service\FilterService;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -16,28 +19,41 @@ class ProductorRepository extends DoctrineEntityRepository
         parent::__construct($registry, Productor::class);
     }
 
-    //    /**
-    //     * @return Productor[] Returns an array of Productor objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function allQuery(): QueryBuilder
+    {
+        return $this->createQueryBuilder('productor')
+            ->select(['productor']);
+    }
 
-    //    public function findOneBySomeField($value): ?Productor
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function paginateAndFilter(FilterService $filterService): PaginatorInterface
+    {
+        $queryBuilder = $this->allQuery();
+        $filterService->apply($queryBuilder);
+
+        return $this->paginator($queryBuilder);
+    }
+
+    public function downloadAndFilter(FilterService $filterService): iterable
+    {
+        $queryBuilder = $this->allQuery()
+            ->addSelect('productor.name as parametroName')
+            ->addSelect('productor.alias as parametroAlias')
+            ->addSelect('productor.isActive as parametroIsActive');
+
+        $filterService->apply($queryBuilder);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    public function allShared(): array
+    {
+        return $this->createQueryBuilder('productor')
+            ->select('productor.uuid as id')
+            ->addSelect('productor.name as name')
+            ->addSelect('productor.name as alias')
+            ->where('productor.isActive = true')
+            ->orderBy('productor.name', 'asc')
+            ->getQuery()
+            ->getResult();
+    }
 }
